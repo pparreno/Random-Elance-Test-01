ebcb9b6b3351157b4cd322ca69a83399f34bd70a
Paul John B. Parreno <paul.john.parreno@gmail.com>
2014-11-19 17:05:29 +0800

    adding new file, FileReader for file reading operations

---
+++ ElanceTest/FileReader.m
@@ -0,0 +1,13 @@
+//
+//  FileReader.m
+//  ElanceTest
+//
+//  Created by Paul John Parreno on 11/19/14.
+//  Copyright (c) 2014 Paul John Parreno. All rights reserved.
+//
+
+#import "FileReader.h"
+
+@implementation FileReader
+
+@end
